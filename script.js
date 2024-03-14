69650abf1688edfa15b5bc02fc9cd680640cbc74
G-Algrithmic <omondi@InaccurateAngel.com>
2024-03-14 23:49:57 +0300

    Features: scroll-behaviour update, elements scroll to the height of the navigation

---
+++ script.js
@@ -0,0 +1,10 @@
+const navigation = 
+
+    document.querySelector("div");
+
+const navigationHeight = 
+
+    navigation.offsetHeight;
+
+    document.documentElement.style.setProperty("--scroll-padding",navigationHeight + "px");
+
